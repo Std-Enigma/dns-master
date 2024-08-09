d3af0e4304a5891719db859270a2163293ddc600
Std-Enigma <std.ilia.bahrebar@gmail.com>
2024-08-09 11:14:56 +0330

    feat(dns_master_cli): add helper functions

--- dns_master_cli.py
+++ dns_master_cli.py
@@ -16,5 +16,23 @@ app = typer.Typer(
     chain=True,
 )
 
+
+def print_fail_message(message: str) -> None:
+    console.print(
+        f":x: {message}.",
+        style="bold italic red",
+        justify="center",
+    )
+
+
+def log_error(e: Exception) -> None:
+    console.log(
+        e, emoji=True, highlight=True, style="bold italic red", justify="center"
+    )
+
+
+def log_failed_operation(message: str, e: Exception) -> None:
+    print_fail_message(f"{message} Please check the error message below for details")
+    log_error(e)
 if __name__ == "__main__":
     app()
