0683d8f4d9a81372591d7c5cdff6dcb4008acdb0
Std-Enigma <std.ilia.bahrebar@gmail.com>
2024-08-09 11:13:58 +0330

    feat(dns_master_cli): typer app initialization

--- dns_master_cli.py
+++ dns_master_cli.py
@@ -1,7 +1,20 @@
+from typing import Annotated, Optional
+
+import typer
 from rich.console import Console
+from rich.table import Table
 
 import config
 from db_manager import DataBaseManager
 
 console = Console()
 db_manager = DataBaseManager(config.USER_DATA_DIRECTORY)
+app = typer.Typer(
+    name="dns-master",
+    rich_markup_mode="rich",
+    no_args_is_help=True,
+    chain=True,
+)
+
+if __name__ == "__main__":
+    app()
