5945c94cb55caeb6fb881ef112c6e82885a3ecec
Std-Enigma <std.ilia.bahrebar@gmail.com>
2024-08-09 11:15:34 +0330

    feat(dns_master_cli): implement add command

--- dns_master_cli.py
+++ dns_master_cli.py
@@ -34,5 +34,47 @@ def log_error(e: Exception) -> None:
 def log_failed_operation(message: str, e: Exception) -> None:
     print_fail_message(f"{message} Please check the error message below for details")
     log_error(e)
+
+
+@app.command(
+    name="add",
+)
+def add_config(
+    identifier: Annotated[
+        str,
+        typer.Argument(),
+    ],
+    primary_address: Annotated[str, typer.Argument()],
+    secondary_address: Annotated[
+        Optional[str],
+        typer.Argument(),
+    ] = None,
+    description: Annotated[
+        Optional[str],
+        typer.Argument(),
+    ] = None,
+    list_after: Annotated[
+        bool,
+        typer.Option(
+            "--list",
+            "-l",
+        ),
+    ] = False,
+) -> None:
+    try:
+        db_manager.add_config(
+            identifier, primary_address, secondary_address, description
+        )
+    except Exception as e:
+        log_failed_operation("Failed to add the configuration", e)
+    else:
+        if list_after:
+            list_configs(identifier)
+
+        console.print(
+            f":heavy_plus_sign: Configuration '{identifier}' has been successfully added.",
+            style="bold italic green",
+            justify="center",
+        )
 if __name__ == "__main__":
     app()
